523a7bc31acc776069725c47c317426a57634b61
kgndnc <hkagandnc@gmail.com>
2023-12-31 23:47:21 +0300

    create custom interfaces with cmake

--- service_client/service_client/client_member_function.py
+++ service_client/service_client/client_member_function.py
@@ -1,6 +1,7 @@
 import sys
 
 from example_interfaces.srv import AddTwoInts
+from tutorial_interfaces.srv import AddThreeInts
 import rclpy
 from rclpy.node import Node
 
@@ -9,16 +10,17 @@ class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__("minimal_client_async")
-        self.cli_ = self.create_client(AddTwoInts, 'add_two_ints')
+        self.cli_ = self.create_client(AddThreeInts, 'add_three_ints')
 
         while not self.cli_.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
 
-        self.req_ = AddTwoInts.Request()
+        self.req_ = AddThreeInts.Request()
 
-    def send_request(self, a, b):
+    def send_request(self, a, b, c):
         self.req_.a = a
         self.req_.b = b
+        self.req_.c = c
         self.future = self.cli_.call_async(self.req_)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
@@ -28,11 +30,12 @@ def main():
     rclpy.init()
 
     minimal_client = MinimalClientAsync()
-    response = minimal_client.send_request(int(sys.argv[1]), int(sys.argv[2]))
+    response = minimal_client.send_request(
+        int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 
     minimal_client.get_logger().info(
-        'Result of add_two_ints: for %d + %d = %d' %
-        (int(sys.argv[1]), int(sys.argv[2]), response.sum))
+        'Result of service: for %d + %d + %d = %d' %
+        (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), response.sum))
 
     minimal_client.destroy_node()
     rclpy.shutdown()
--- service_client/service_client/service_member_function.py
+++ service_client/service_client/service_member_function.py
@@ -1,5 +1,5 @@
 from example_interfaces.srv import AddTwoInts
-from example_interfaces.action import Fibonacci
+from tutorial_interfaces.srv import AddThreeInts
 
 import rclpy
 from rclpy.node import Node
@@ -10,11 +10,12 @@ class MinimalService(Node):
     def __init__(self):
         super().__init__("minimal_service")
         self.srv = self.create_service(
-            AddTwoInts, "add_two_ints", self.add_two_ints_callback)
+            AddThreeInts, "add_three_ints", self.add_three_ints_callback)
 
-    def add_two_ints_callback(self, request, response):
-        response.sum = request.a + request.b
-        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+    def add_three_ints_callback(self, request, response):
+        response.sum = request.a + request.b + request.c
+        self.get_logger().info('Incoming request\na: %d b: %d c: %d' %
+                               (request.a, request.b, request.c))
 
         return response
 
