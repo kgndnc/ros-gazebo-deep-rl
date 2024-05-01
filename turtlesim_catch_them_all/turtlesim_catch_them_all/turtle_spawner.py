26f4138ebc82a750d42b46231df7b04693e382ab
kgndnc <hkagandnc@gmail.com>
2024-05-01 17:44:33 +0300

    create service to "catch turtles" which calls the "kill" service advertised by "turtlesim"

--- turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
+++ turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_spawner.py
@@ -2,11 +2,13 @@
 
 import rclpy
 from rclpy.node import Node
-from turtlesim.srv import Spawn
+from turtlesim.srv import Spawn, Kill
 import random
 from math import pi
 from functools import partial
 from tutorial_interfaces.msg import Turtle, TurtleArray
+from tutorial_interfaces.srv import CatchTurtle
+
 
 """
 Call the /spawn service to create a new turtle (choose random coordinates between 0.0 and 11.0 for both x and y),
@@ -20,11 +22,59 @@ class TurtleSpawner(Node):
         super().__init__("turtle_spawner")
         self.create_timer(2.0, self.call_spawn_service)
 
-        # self.alive_turtles_ = TurtleArray()
         self.alive_turtles_ = []
 
         self.alive_turtles_publisher_ = self.create_publisher(
             TurtleArray, "alive_turtles", 10)
+
+        self.catch_turtle_service_ = self.create_service(
+            CatchTurtle, "catch_turtle", self.catch_turtle)
+
+    """
+    Handle a service server to “catch” a turtle, which means to call the 
+    /kill service and remove the turtle from the array of alive turtles.
+    """
+
+    def catch_turtle(self, request, response):
+        self.get_logger().info("Inside catch_turtle service")
+        print(request)
+
+        client = self.create_client(Kill, "kill")
+        req = Kill.Request()
+        req.name = request.turtle_name
+        future = client.call_async(request=req)
+        future.add_done_callback(partial(
+            self.callback_kill_done, killed_turtle_name=req.name
+        ))
+
+        response.success = True
+        return response
+
+    def callback_kill_done(self, future, killed_turtle_name):
+        self.get_logger().info(f"Killing turtle {killed_turtle_name}")
+
+        try:
+            response = future.result()
+
+            index = None
+
+            for i, turtle in enumerate(self.alive_turtles_):
+                if turtle.name == killed_turtle_name:
+                    index = i
+                    break
+
+            del self.alive_turtles_[index]
+
+            self.publish_alive_turtles()
+
+        except Exception as e:
+            self.get_logger().error("Service call failed %r" % (e,))
+
+        return
+
+    """
+    Publish the list of currently alive turtles with coordinates on a topic /alive_turtles.
+    """
 
     def publish_alive_turtles(self):
         msg = TurtleArray()
@@ -50,7 +100,8 @@ class TurtleSpawner(Node):
         future = client.call_async(request=request)
 
         future.add_done_callback(
-            partial(self.callback_spawn_done, new_turtle=new_turtle))
+            partial(self.callback_spawn_done, new_turtle=new_turtle)
+        )
 
     def callback_spawn_done(self, future, new_turtle: Turtle):
         try:
@@ -67,18 +118,6 @@ class TurtleSpawner(Node):
 
         except Exception as e:
             self.get_logger().error("Service call failed %r" % (e,))
-
-    """
-    Publish the list of currently alive turtles with coordinates on a topic /alive_turtles.
-    """
-
-#     def publish_alive_turtles(self):
-#         publisher_ = self.create_publisher(TurtleArray, "alive_turtles", 10)
-#
-#         msg = TurtleArray()
-#         msg = self.alive_turtles_
-#
-#         publisher_.publish(msg)
 
 
 def main(args=None):
