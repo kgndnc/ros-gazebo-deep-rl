f4794990c0d45d9b92fceb5b7460ff0f71f10520
kgndnc <hkagandnc@gmail.com>
2024-05-01 17:45:23 +0300

    create client to call "catch_turtle" service advertised by "turtle_spawner" node

--- turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
+++ turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
@@ -2,11 +2,35 @@
 
 import rclpy
 from rclpy.node import Node
+from threading import Timer
+from functools import partial
+
+from tutorial_interfaces.srv import CatchTurtle
 
 
 class TurtleController(Node):
     def __init__(self):
         super().__init__("turtle_controller")
+
+        self.catch_turtle_client_ = self.create_client(
+            CatchTurtle, "catch_turtle")
+
+        self.call_catch_turtle_service()
+
+    def call_catch_turtle_service(self):
+        self.get_logger().info("Calling catch_turtle service")
+        request = CatchTurtle.Request()
+        request.turtle_name = "turtle2"
+
+        future = self.catch_turtle_client_.call_async(request=request)
+        future.add_done_callback(partial(self.callback_catch_done))
+
+    def callback_catch_done(self, future):
+
+        response = future.result()
+
+        self.get_logger().info(
+            f"Callback catch done here's the result {response.success}")
 
 
 def main(args=None):
